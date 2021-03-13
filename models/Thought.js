30bafe0b5fb840e04f043f500ad9a7dbc7628889
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-13 00:29:33 -0800

    Feat: Set up the Thought.js file, note to visit it later to add another const at the top

--- models/Thought.js
+++ models/Thought.js
@@ -1,20 +1,42 @@
-// thoughtText
+const { Schema, model } = require('mongoose');
 
-// String
-// Required
-// Must be between 1 and 280 characters
-// createdAt
+// Thoughts 
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            // Must be between 1-280
+            minLength: 1,
+            maxLength: 120
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        // Array of nested documents created with the reactionSchema
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false,
+    }
+);
 
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-// username (The user that created this thought)
+// reactionCount virtual, retrieves the length of the thought's reactions array field on query.
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
-// String
-// Required
-// reactions (These are like replies)
+const Thought = model('Thought', ThoughtSchema);
 
-// Array of nested documents created with the reactionSchema
-// Schema Settings
-
-// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+module.exports = Thought;

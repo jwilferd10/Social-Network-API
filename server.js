d743240d38c5339e7004e3c92376b9cdfe9c4805
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-12 19:16:48 -0800

    Feat: server.js set-up complete-ish

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Make sure to use this to log mongo queries being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`You're now connected on localhost:${PORT}`));

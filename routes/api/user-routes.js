2c18b14c83392d8884a265294582048c6ab12881
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-15 00:40:03 -0700

    Feat: User-routes done

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,34 @@
 const router = require('express').Router();
 
+// Router pulling from this const
 const {
-        
-}
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    createFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
+
+// For getting all users and creating a user
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// Route specifically finding things by id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+// Friend Routes
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(createFriend)
+    .delete(deleteFriend)
+
+module.exports = router;

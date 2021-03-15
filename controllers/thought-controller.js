14f378a366aae5df33579b1b7ac843aa51fb1cb9
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-14 22:37:43 -0700

    Feat: Skeleton of thought-controller set-up

---
+++ controllers/thought-controller.js
@@ -0,0 +1,21 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    // GET all available thoughts
+
+    // FGET one thought by _id
+
+    /* 
+    POST to create a new thought
+        example data
+        {
+            "thoughtText": "Here's a cool thought...",
+            "username": "lernantino",
+            "userId": "5edff358a0fcb779aa7b118b"
+        }
+    */
+
+    // PUT to update a thought by its _id
+    
+    // Delete to remove a thought by its _id
+}
